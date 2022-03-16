5094d607753727229d4ccc84686ae3d7b87982f6
Dinicescu Radu <rodede.radu@gmail.com>
2022-03-16 14:43:08 +0200

    organised code

--- Laboratoare/Lab01/functiiLista.c
+++ Laboratoare/Lab01/functiiLista.c
@@ -32,3 +32,92 @@ void DistrugeL(TLista* aL)
     free(aux);
   }
 }
+
+TLista CitireL(int *lg)     /* construieste lista cu elemente citite */
+{ TLista L = NULL, u, aux;
+  int x;
+  char ch;
+  for(*lg = 0; scanf("%i", &x) == 1; )
+  {
+    aux = AlocCelula(x);           /* incearca inserarea valorii citite */
+    if(!aux) return L;              /* alocare esuata => sfarsit citire */
+    if(L == NULL){
+      L = aux;
+    } else {
+      //lista ordonata crescator
+      
+      if(aux->info < L->info) {
+        aux->urm = L;
+        L = aux;
+      } else {
+        u = L;
+        while (u->urm != NULL && u->urm->info <= aux->info)
+        u = u->urm;
+
+        aux->urm = u->urm;
+        u->urm = aux;
+      }
+    }
+    
+    (*lg)++;
+  }
+
+  while((ch=getchar()) != EOF && ch != '\n');
+  return L;                     /* intoarce lista rezultat */
+}
+void f1(TLista x, int div) {
+  int i = 1;
+  
+  for ( ;x != NULL && x->urm != NULL; x = x->urm->urm) {
+    if (x->info % div == 0 && x->urm->info % div == 0) {
+      printf("Indicii: %d, %d; Numerele %d, %d\n", i, i+1, x->info, x->urm->info);
+    }
+    i += 2;
+  }
+}
+
+int compare(TLista A, TLista B) {
+  for (TLista I = A; I != NULL; I = I->urm) {
+    int sem = 0;
+    for(TLista II = B; II != NULL; II = II->urm) {
+      if(I->info == II->info)
+        sem = 1;
+    }
+    if (!sem) return 0;
+  }
+
+  for (TLista I = B; I != NULL; I = I->urm) {
+    int sem = 0;
+    for(TLista II = A; II != NULL; II = II->urm) {
+      if(I->info == II->info)
+        sem = 1;
+    }
+    if (!sem) return 0;
+  }
+
+  return 1;
+}
+
+int f3(TLista x, int *inc, int *sf) {
+  *inc = 0;
+  *sf = 0;
+
+  int ninc = 1, nsf = 0;
+  for (; x != NULL; x = x->urm) {
+    nsf++;
+    TLista nxt = x->urm;
+    if(nxt == NULL || x->info > nxt->info) {
+      if(nsf - ninc) {
+        *sf = nsf;
+        *inc = ninc;
+      }
+      ninc = nsf + 1;
+    }
+  }
+  if (*sf - *inc == 0) {
+    *sf = -1;
+    *inc = -1;
+    return 0;
+  }
+  return *sf - *inc + 1;
+}
