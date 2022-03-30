8516aa709964b9d3a48c1ff193b9782db6109634
Dinicescu Radu <rodede.radu@gmail.com>
2022-03-30 15:58:57 +0300

    arranged lab04

---
+++ Laboratoare/Lab04/carti.h
@@ -0,0 +1,12 @@
+typedef struct {
+	char titlu[50];
+	char autor[30];
+	char editura[30];
+} TCarte;
+
+
+int codHash(void * element);
+void afisareCarte(void * element);
+TLG citesteListaCarti(char * numeFisier);
+int cmpCarte(void * e1, void * e2);
+TH * GenerareHash(TLG listaCarti);
---
+++ Laboratoare/Lab04/functiiCarte.c
@@ -0,0 +1,94 @@
+#include "carti.h"
+#include "thash.h"
+#include "tlg.h"
+int codHash(void * element)
+{
+	TCarte * carte = (TCarte *) element;
+	char * autor = carte->autor;
+	return *autor - 'A';
+}
+
+void afisareCarte(void * element)
+{
+	TCarte * carte = (TCarte *) element;
+	printf("%s - %s - %s\n", carte->autor, carte->titlu, carte->editura);
+}
+
+TLG citesteListaCarti(char * numeFisier) {
+	FILE *f;
+	char * line = NULL;
+	size_t len = 0;
+	TLG L = NULL;
+
+	f = fopen(numeFisier, "rt");
+	if (f == NULL)
+		return NULL;
+
+	while (getline(&line, &len, f) != -1) {
+		char * autor = strtok(line, ":");
+		char * titlu = strtok(NULL, ":");
+		char * editura = strtok(NULL, ":");
+
+		if (editura[strlen(editura) - 1] == '\n')
+			editura[strlen(editura) - 1] = '\0';
+
+		TCarte * carte = malloc(sizeof(TCarte));
+		if (carte == NULL)
+			return L;
+
+		strcpy(carte->autor, autor);
+		strcpy(carte->titlu, titlu);
+		strcpy(carte->editura, editura);
+		Ins_IncLG(&L, (void *) carte);
+    }
+	fclose(f);
+	return L;
+}
+
+int cmpCarte(void * e1, void * e2)
+{
+	TCarte * carte1 = (TCarte *) e1;
+	TCarte * carte2 = (TCarte *) e2;
+
+	if (strcmp(carte1->titlu, carte2->titlu) != 0)
+		return 0;
+
+	if (strcmp(carte1->autor, carte2->autor) != 0)
+		return 0;
+
+	if (strcmp(carte1->editura, carte2->editura) != 0)
+		return 0;
+
+	return 1;
+}
+
+TH * GenerareHash(TLG listaCarti)
+{
+    TH *h = NULL;
+	TLG p;
+    TCarte * carte;
+	int rez;
+
+	/* calcul dimensiuni maxime pt tabela hash */
+	size_t M = ('Z'-'A');
+
+	/* initializare tabela hash */
+	h = (TH *) InitTH(M, codHash);
+	if(h == NULL)
+		return NULL;
+
+	for(p = listaCarti; p != NULL; p = p->urm) {
+        carte = (TCarte *) malloc(sizeof(TCarte));
+        if(carte == NULL)
+						return h;
+
+        memcpy(carte, p->info, sizeof(TCarte));
+		rez = InsTH(h, carte, cmpCarte);
+        if(!rez) {
+						free(carte);
+						return h;
+		}
+	}
+
+	return h;
+}
